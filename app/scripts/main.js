2256d74a52b197d7bfeec3f3587d0fa9443cd4f3
William Beard <disenno@gmail.com>
2014-02-27 17:18:20 -0600

    Big changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+  $("#navMenu").click(function(evt) {
+    $(this).children(".fa").toggleClass("fa-rotate-180");
+  });
+
+  $("#firstCall").click(function(evt) {
+    evt.preventDefault();
+    $("#salesModal").modal('show');
+  });
+
+  $("#secondCall").click(function(evt) {
+    evt.preventDefault();
+    $("#salesModal").modal('show');
+  });
+
+  $("#initBrowse").click(function(evt) {
+    evt.preventDefault();
+
+    var topPosition = $("#paymentProcessing").position().top;
+
+    $("html, body").animate({
+      scrollTop: topPosition - 50
+    });
+
+  });
+
+});
